1a3095ae9dbcb7eef939ef28a6a417d8d702621e
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 14:39:24 -0300

    adicionando ex11

---
+++ ex11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    int n = 125;
+    printf("n = %.8d (decimal)\n",n);
+    printf("n = %.8o (octal)\n",n);
+    printf("n = %.8X (hexadecimal)\n",n);
+    system("pause");
+    return 0;
+}
