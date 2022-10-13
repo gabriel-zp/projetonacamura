fd649af068b8b05601a1a1c8061c27162005adf9
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 14:42:57 -0300

    adicionando ex12

---
+++ ex12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    float n = 123.45678;
+    printf("n = %.3f\n",n);
+    printf("n = %.5f\n",n);
+    printf("n = %.5e\n",n);
+    printf("n = %.5g\n",n);
+    system("pause");
+    return 0;
+}
