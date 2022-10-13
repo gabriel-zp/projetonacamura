a280859699e5a00c4cb1b8e9fb95fca53f1709c7
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 14:22:18 -0300

    adicionando ex7

---
+++ ex7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    int n = 5;
+    //sem sinal
+    printf("n = %d\n",n);
+    //com sinal
+    printf("n = %+d\n", n);
+    system("pause");
+    return 0;
+}
