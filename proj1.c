12d5ab2797fb7fa0b41098466489c6108f3ccd25
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 13:49:03 -0300

    atualizando git status git commit

--- proj1.c
+++ proj1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main()
-{
-    printf("tese11");
+#include <stdlib.h>
+int main(){
+    int n = 125;
+    float f = 5.25;
+    double d = 10.53;
+    char letra = 'A';
+    char palavra[10] = "programa";
+    printf("Valor inteiro: %d\n",n);
+    printf("Valor inteiro: %i\n", n);
+    printf("Valor real: %f\n, f");
+    printf("Valor real: %f\n, d");
+    printf("Caractere: %c\n, letra");
+    printf("Valor numérico do caractere: %d\n", letra);
+    printf("Palavra: %s\n", palavra);
+    return 0; 
 }
