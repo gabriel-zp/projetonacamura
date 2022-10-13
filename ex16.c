1160280682bda36bb2635e3f10ec272f7be949a1
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 14:59:09 -0300

    adicionando exercício final.

---
+++ ex16.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    printf("Caractere 3D: \x3D \n");
+    printf("Caractere 5A: \x5A \n");
+    printf("Caractere 44: \044 \n");
+    printf("Caractere 105: \105 \n");
+    system("pause");
+    return 0;
+}
