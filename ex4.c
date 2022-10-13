7776209fbbedb94ed1b42a4373f2b0ef1840e4c3
gabriel z <gabizpiccinini@gmail.com>
2022-10-13 14:07:45 -0300

    adicionando ex4

---
+++ ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    unsigned int n = 2147483647;
+    printf("Valor real: %d\n",n);
+    printf("Valor real: %u\n",n);
+    n = n + 1;
+    printf("Valor real: %d\n",n);
+    printf("Valor real %u\n",n);
+    printf("Endereco de n = %p\n", &n);
+    system("pause");
+    return 0;
+}
